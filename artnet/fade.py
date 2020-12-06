6ef1d8780c6f2ef4c244d8ca0e14a2d1ec2803b0
zenonymous <blueberrysky@gmail.com>
2020-12-06 18:43:24 +0000

    ok

--- artnet/fade.py
+++ artnet/fade.py
@@ -11,11 +11,12 @@ async def functie():
     # dit zou een range van channels kunnen requesten
     # channel = universe.get_channel('1/3')   
     channel  = universe.add_channel(start=1, width=3)
+    channel1 = universe.add_channel(start=4, width=3)
 
     # Fade channel to 255,0,0 in 5s
     # The fade will automatically run in the background
-    channel.add_fade([255,0,0], 5000)   
-
+    channel.add_fade([255,0,0], 1000)   
+    channel1.add_fade([255,0,0], 2000)
     # this can be used to wait till the fade is complete
 
     await channel.wait_till_fade_complete()
