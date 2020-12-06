7d42fe8cd488cae3e7cc05c2a6b712e716e8fab7
zenonymous <blueberrysky@gmail.com>
2020-12-06 18:31:03 +0000

    async

--- artnet/fade.py
+++ artnet/fade.py
@@ -1,20 +1,24 @@
 from pyartnet import ArtNetNode
+import asyncio
+
 
 node = ArtNetNode('2.0.0.2')
-await node.start()
+async def functie():
+    await node.start()
 
-universe = node.add_universe(0)
+    universe = node.add_universe(0)
 
-# dit zou een range van channels kunnen requesten
-# channel = universe.get_channel('1/3')   
-channel  = universe.add_channel(start=1, width=3)
+    # dit zou een range van channels kunnen requesten
+    # channel = universe.get_channel('1/3')   
+    channel  = universe.add_channel(start=1, width=3)
 
-# Fade channel to 255,0,0 in 5s
-# The fade will automatically run in the background
-channel.add_fade([255,0,0], 5000)   
+    # Fade channel to 255,0,0 in 5s
+    # The fade will automatically run in the background
+    channel.add_fade([255,0,0], 5000)   
 
-# this can be used to wait till the fade is complete
-await channel.wait_till_fade_complete()
+    # this can be used to wait till the fade is complete
 
+    await channel.wait_till_fade_complete()
 
+asyncio.run(functie())
 
