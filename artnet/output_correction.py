0a6eaa52f8270f2cc2eab5d67ce798cbbe6020cf
zenonymous <blueberrysky@gmail.com>
2020-12-06 21:59:47 +0000

    removing output_correction.py

--- artnet/output_correction.py
+++
@@ -1,9 +0,0 @@
-from pyartnet import ArtNetNode, output_correction
-
-node = ArtNetNode('2.0.0.2')
-
-universe = node.add_universe(0)
-universe.output_correction = output_correction.quadratic  # quadratic will be used for all channels
-
-channel = universe.add_channel(start=1, width=3)
-channel.output_correction = output_correction.cubic       # this channel will use cubic
