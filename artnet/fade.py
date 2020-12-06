9af92c39003a2c503faaaa58f07fc4c21c3a0190
zenonymous <blueberrysky@gmail.com>
2020-12-06 19:35:42 +0000

    okok

--- artnet/fade.py
+++ artnet/fade.py
@@ -7,7 +7,7 @@ async def functie():
     await node.start()
 
     universe = node.add_universe(0)
-
+    multiverse = node.add_universe(1)
     # dit zou een range van channels kunnen requesten
     # channel = universe.get_channel('1/3')  
     one = random.randint(0,500)
@@ -19,25 +19,45 @@ async def functie():
     channel2 = universe.add_channel(start=three, width=6)
     channel3 = universe.add_channel(start=four, width=6)
 
+    channel4 = multiverse.add_channel(start=four, width=6)
+    channel5 = multiverse.add_channel(start=three, width=6)
+    channel6 = multiverse.add_channel(start=two, width=6)
+    channel7 = multiverse.add_channel(start=one, width=6)
 
     ha = random.randint(0,25)
     ja = random.randint(0,25)
+    bla = random.randint(0,50)
 
+    timer1 = random.randint(0,4000)
+    timer2 = random.randint(0,5000)
+    timer3 = random.randint(0,6000)
+    timer4 = random.randint(0,2000)
 
     # Fade channel to 255,0,0 in 5s
     # The fade will automatically run in the background
-    channel.add_fade([ha,ja,0,5,3,2], 2000)   
-    channel1.add_fade([255,23,0,5,4,2], 2000)
-    channel2.add_fade([0,255,23,43,6,9], 3000)
-    channel3.add_fade([35,92,37,34,54,24], 3000)
+    channel.add_fade([ha,ja,0,bla,3,2], timer1)   
+    channel1.add_fade([255,23,ja,5,bla,2], timer2)
+    channel2.add_fade([ha,255,bla,43,6,9], timer3)
+    channel3.add_fade([35,ja,37,34,bla,24], timer4)
+
+    channel4.add_fade([ja,bla,ha,bla,ja,ha], timer4)
+    channel5.add_fade([bla,ha,ja,bla,ha,ja], timer3)
+    channel6.add_fade([ja,ha,bla,ja,ha,bla], timer2)
+    channel7.add_fade([bla,bla,ja,ja,ha,ha], timer1)
+
     # this can be used to wait till the fade is complete
 
     await channel.wait_till_fade_complete()
     
-    channel.add_fade([0,0,0,0,0,0], 3000)
-    channel1.add_fade([0,0,0,0,0,0], 5000)
-    channel2.add_fade([0,0,0,0,0,0], 4000)
-    channel3.add_fade([0,0,0,0,0,0], 1000)
+    channel.add_fade([0,0,0,0,0,0], timer4)
+    channel1.add_fade([0,0,0,0,0,0], timer3)
+    channel2.add_fade([0,0,0,0,0,0], timer2)
+    channel3.add_fade([0,0,0,0,0,0], timer1)
+    channel4.add_fade([0,0,0,0,0,0], timer1)
+    channel5.add_fade([0,0,0,0,0,0], timer2)
+    channel6.add_fade([0,0,0,0,0,0], timer3)
+    channel7.add_fade([0,0,0,0,0,0], timer4)
+
 
     await channel1.wait_till_fade_complete()
 
