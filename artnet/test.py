76cd977164763f333283ebaeb22b027e60b16fd3
buymecookies <75701354+buymecookies@users.noreply.github.com>
2020-12-09 21:36:10 +0100

    Update test.py

--- artnet/test.py
+++ artnet/test.py
@@ -3,7 +3,7 @@ import asyncio
 import time
 
 def Green():
-    print("255,0,0,255,0,0")
+    print("[255,0,0,255,0,0]")
     
 def Red():
     print("0,255,0,0,255,0")
@@ -349,7 +349,7 @@ async def functie():
 # Fade channel to 255,0,0 in 5s
 # The fade will automatically run in the background
 #stap 1
-    fixturea1.add_fade(["Green"], 1000)
+    fixturea1.add_fade(Green(), 1000)
     fixtureb1.add_fade([255,0,0,255,0,0], 1000)
     fixturec1.add_fade([255,0,0,255,0,0], 1000)
     fixtured1.add_fade([255,0,0,255,0,0], 1000)
