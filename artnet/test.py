9395d97f392e3637b5456577bfd9fe1874dca7d6
buymecookies <75701354+buymecookies@users.noreply.github.com>
2020-12-09 21:59:59 +0100

    Update test.py

--- artnet/test.py
+++ artnet/test.py
@@ -315,7 +315,7 @@ fixturef47 = universe3.add_channel(start=343, width=6)
 fixturef48 = universe3.add_channel(start=349, width=6)
 
 async def GREEN():
-    print("255,0,0,255,0,0"))
+    print("255,0,0,255,0,0")
 
 
 
